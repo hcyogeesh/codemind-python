9974274d3812c254a9def73b683696f5e1614d9d
Yogeesha H C <hcyogeesh@gmail.com>
2022-12-24 21:14:04 +0530

    def value(r): 	if (r == 'I'): 		return 1 	if (r == 'V'): 		return 5 	if (r == 'X'): 		return 10 	if (r == 'L'): 		return 50 	if (r == 'C'): 		return 100 	if (r == 'D'): 		return 500 	if (r == 'M'): 		return 1000 	return -1
    def romanToDecimal(str):
    	res = 0
    	i = 0
    
    	while (i < len(str)):
    		s1 = value(str[i])
    		if (i + 1 < len(str)):
    			s2 = value(str[i + 1])
    			if (s1 >= s2):
    				res = res + s1
    				i = i + 1
    			else:
    				res = res + s2 - s1
    				i = i + 2
    		else:
    			res = res + s1
    			i = i + 1
    
    	return res
    
    str=input()
    print(romanToDecimal(str))

--- Roman_to_Integer.py
+++ Roman_to_Integer.py
@@ -1,3 +1,4 @@
+
 def value(r):
 	if (r == 'I'):
 		return 1
@@ -20,26 +21,13 @@ def romanToDecimal(str):
 	i = 0
 
 	while (i < len(str)):
-
-		# Getting value of symbol s[i]
 		s1 = value(str[i])
-
 		if (i + 1 < len(str)):
-
-			# Getting value of symbol s[i + 1]
 			s2 = value(str[i + 1])
-
-			# Comparing both values
 			if (s1 >= s2):
-
-				# Value of current symbol is greater
-				# or equal to the next symbol
 				res = res + s1
 				i = i + 1
 			else:
-
-				# Value of current symbol is greater
-				# or equal to the next symbol
 				res = res + s2 - s1
 				i = i + 2
 		else:
@@ -48,5 +36,5 @@ def romanToDecimal(str):
 
 	return res
 
-n=input()
-print(romanToDecimal(n))
+str=input()
+print(romanToDecimal(str))
