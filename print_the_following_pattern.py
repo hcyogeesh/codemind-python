4a4e891b1d1355cfad3be5f4d071f3f55a4c001f
Yogeesha H C <hcyogeesh@gmail.com>
2022-12-26 10:13:26 +0530

    rows=int(input()) ch='A' for i in range(1, rows + 1):     for j in range(1, rows+1):         print(ch, end=" ")     ch=chr(ord(ch)+1)     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,6 +1,7 @@
-n = int(input())
-
-for i in range(n, 0, -1):
-    for j in range(1,i+1):
-        print(str(j), end="")
-    print("")
+rows=int(input())
+ch='A'
+for i in range(1, rows + 1):
+    for j in range(1, rows+1):
+        print(ch, end=" ")
+    ch=chr(ord(ch)+1)
+    print()
